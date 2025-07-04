bb3bf3045b1eeaef37bda5f11c5c2de7359e7dd8
tiendat3008 <trantiendat1679@gmail.com>
2025-07-04 15:57:33 +0700

    set up sanity, login with github, search function

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  dangerouslyAllowSVG: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
